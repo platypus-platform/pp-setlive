15ef294174154535c934e87234ed7ba7df18d91a
Xavier Shay <xavier@squareup.com>
2014-08-30 15:29:26 -0700

    Extract duplicated store logic.

--- main.go
+++ main.go
@@ -1,9 +1,8 @@
 package main
 
 import (
-	"errors"
-	"github.com/platypus-platform/pp-kv-consul"
 	. "github.com/platypus-platform/pp-logging"
+	"github.com/platypus-platform/pp-store"
 	"gopkg.in/yaml.v1"
 	"io/ioutil"
 	"os"
@@ -30,7 +29,7 @@ func main() {
 		RunitPath:        "/var/service",
 	}
 
-	c := make(chan IntentNode)
+	c := make(chan pp.IntentNode)
 	var wg sync.WaitGroup
 	wg.Add(1)
 	go func() {
@@ -42,7 +41,7 @@ func main() {
 		}
 	}()
 
-	err = pollOnce(hostname, c)
+	err = pp.PollOnce(hostname, c)
 	close(c)
 	wg.Wait()
 	if err != nil {
@@ -51,7 +50,7 @@ func main() {
 	}
 }
 
-func setLive(config SetliveConfig, app IntentApp) {
+func setLive(config SetliveConfig, app pp.IntentApp) {
 	version := app.ActiveVersion()
 
 	if version == "" {
@@ -105,7 +104,7 @@ func setLive(config SetliveConfig, app IntentApp) {
 	}
 }
 
-func configureServiceBuilder(config SetliveConfig, app IntentApp) error {
+func configureServiceBuilder(config SetliveConfig, app pp.IntentApp) error {
 	sb := map[string]interface{}{}
 	sb[app.Name] = map[string][]string{
 		"run": []string{
@@ -133,115 +132,4 @@ func configureServiceBuilder(config SetliveConfig, app IntentApp) error {
 		return err
 	}
 	return nil
-}
-
-type IntentApp struct {
-	Name     string
-	Versions map[string]string
-	Basedir  string
-}
-
-type IntentNode struct {
-	Apps map[string]IntentApp
-}
-
-func (app *IntentApp) ActiveVersion() string {
-	for id, status := range app.Versions {
-		if status == "active" {
-			return id
-		}
-	}
-	return ""
-}
-
-// TODO: This is duplicated from pp-preparer. DRY it up.
-func pollOnce(hostname string, c chan IntentNode) error {
-	Info("Polling intent store")
-	kv, _ := ppkv.NewClient()
-	apps, err := kv.List(path.Join("nodes", hostname))
-	if err != nil {
-		return err
-	}
-
-	intent := IntentNode{
-		Apps: map[string]IntentApp{},
-	}
-
-	for appName, data := range apps {
-		Info("Checking spec for %s", appName)
-
-		appData, worked := stringMap(data)
-		if !worked {
-			Fatal("Invalid node data for %s", appName)
-			continue
-		}
-
-		cluster := appData["cluster"]
-		if cluster == "" {
-			Fatal("No cluster key in node data for %s", appName)
-			continue
-		}
-
-		clusterKey := path.Join("clusters", appName, cluster, "versions")
-		configKey := path.Join("clusters", appName, cluster, "deploy_config")
-
-		versions, err := getMap(kv, clusterKey)
-		if err != nil {
-			Fatal("No or invalid data for %s: %s", clusterKey, err)
-			continue
-		}
-
-		deployConfig, err := getMap(kv, configKey)
-		if err != nil {
-			Fatal("No or invalid data for %s: %s", configKey, err)
-			continue
-		}
-
-		basedir := deployConfig["basedir"]
-		if !path.IsAbs(basedir) {
-			Fatal("Not allowing relative basedir in %s", configKey)
-			continue
-		}
-
-		intent.Apps[appName] = IntentApp{
-			Name:     appName,
-			Basedir:  basedir,
-			Versions: versions,
-		}
-	}
-
-	c <- intent
-
-	return nil
-}
-
-func getMap(kv *ppkv.Client, query string) (map[string]string, error) {
-	raw, err := kv.Get(query)
-
-	if err != nil {
-		return nil, err
-	}
-
-	mapped, worked := stringMap(raw)
-	if !worked {
-		return nil, errors.New("Not a string map")
-	}
-
-	return mapped, nil
-}
-
-func stringMap(raw interface{}) (map[string]string, bool) {
-	mapped, worked := raw.(map[string]interface{})
-	if !worked {
-		return nil, false
-	}
-	ret := map[string]string{}
-	for k, v := range mapped {
-		str, worked := v.(string)
-		if !worked {
-			return nil, false
-		}
-		ret[k] = str
-	}
-	return ret, true
 }
