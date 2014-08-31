c9b46d05061b127ff63cde9d0320e5bf45ffd3f4
Xavier Shay <xavier@squareup.com>
2014-08-31 13:08:14 -0700

    Add TODO I missed.

--- main.go
+++ main.go
@@ -88,6 +88,7 @@ func setLive(config SetliveConfig, app pp.IntentApp) {
 
 	if healthy {
 		Info("%s: acquiring lease", app.Name)
+		// TODO: Get minnodes from intent store
 		lease := client.AcquireShutdownLease(app.Name, 0)
 		if lease != nil {
 			func() {
